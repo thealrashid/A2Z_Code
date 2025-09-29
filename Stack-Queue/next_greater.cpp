c5ce90bb2c95ea7e96480ac25c6dee96662bf138
Mamun Al Rashid <mamunalrashid17@gmail.com>
2025-09-29 16:00:37 +0000

    Stack 29/9

---
+++ Stack-Queue/next_greater.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2) {
+    vector<int> res;
+    unordered_map<int, int> nextGreater;
+    stack<int> st;
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+
+    for (int i = 0; i < n2; i++) {
+        while (!st.empty() && nums2[i] > st.top()) {
+            nextGreater[st.top()] = nums2[i];
+            st.pop();
+        }
+        st.push(nums2[i]);
+    }
+
+    for (int i = 0; i < n1; i++) {
+        if (nextGreater.find(nums1[i]) != nextGreater.end()) {
+            res.push_back(nextGreater[nums1[i]]);
+        } else {
+            res.push_back(-1);
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    vector<int> nums1 = {4,1,2};
+    vector<int> nums2 = {1,3,4,2};
+
+    vector<int> res = nextGreaterElement(nums1, nums2);
+
+    for (int itr : res) {
+        cout << itr << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
